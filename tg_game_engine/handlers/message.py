4261ee7ea04fc3725a4608133ac0bfa4df6f7360
vcslav-v <vcslav.v@gmail.com>
2021-06-07 17:24:39 +0300

    test

--- tg_game_engine/handlers/message.py
+++ tg_game_engine/handlers/message.py
@@ -1,9 +1,12 @@
 from tg_game_engine.main import bot
 from tg_game_engine.db import tools
+from time import sleep
 
 
 @bot.message_handler(commands=['start'])
 def start_message(msg):
+    sleep(10)
+    print('wakeup')
     user = tools.get_user(msg.from_user.id)
     bot.send_message(msg.chat.id, user.telegram_id)
 
