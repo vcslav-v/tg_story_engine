d51f42794453c31c40a9baf6be05400f6680aef8
vcslav-v <vcslav.v@gmail.com>
2021-08-04 23:55:29 +0300

    try

--- tg_game_engine/handlers/web.py
+++ tg_game_engine/handlers/web.py
@@ -11,12 +11,15 @@ from tg_game_engine.main import BOT_TOKEN, app, bot
 @app.route('/' + BOT_TOKEN, methods=['POST'])
 @logger.catch
 def getMessage():
-    bot.process_new_updates([
-            telebot.types.Update.de_json(
-                request.stream.read().decode('utf-8')
-            )
-    ])
-    return 'ok', 200
+    try:
+        bot.process_new_updates([
+                telebot.types.Update.de_json(
+                    request.stream.read().decode('utf-8')
+                )
+        ])
+        return 'ok', 200
+    except:
+        return 'incorrect', 400
 
 
 @app.route('/' + BOT_TOKEN, methods=['GET'])
