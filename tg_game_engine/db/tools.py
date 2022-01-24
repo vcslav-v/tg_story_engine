dab2cfd979d67e1c3f45f80183d0663e278f35e9
Vcslav <vcslav.v@gmail.com>
2022-01-24 21:37:57 +0300

    fix

--- tg_game_engine/db/tools.py
+++ tg_game_engine/db/tools.py
@@ -169,7 +169,7 @@ def add_story(db: Session, zip_value):
             for field, value in msg_info:
                 if field == 'photo':
                     new_msg.content_type = field
-                    with zip_value.open(f'media/{value}') as media_file:
+                    with zip_value.open(f'media/{value}', 'rb') as media_file:
                         new_media = models.Media(
                             file_data=media_file.read(),
                             parrent_message=new_msg,
@@ -177,7 +177,7 @@ def add_story(db: Session, zip_value):
                         db.add(new_media)
                 elif field == 'voice':
                     new_msg.content_type = field
-                    with zip_value.open(f'media/{value}') as media_file:
+                    with zip_value.open(f'media/{value}', 'rb') as media_file:
                         new_media = models.Media(
                             file_data=media_file.read(),
                             parrent_message=new_msg,
